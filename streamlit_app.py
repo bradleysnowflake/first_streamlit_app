efd319d8bd319f7fd1460d05bebf74f6203902fe
bradleysnowflake <122900109+bradleysnowflake@users.noreply.github.com>
2023-01-17 10:14:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
-streamlit.title('gg ez')
+streamlit.title('Snowflake Application Builder Test App deployed via Streamlit')
+
+streamlit.header('gg ez')
+
+streamlit.text('Example of display text')
+streamlit.text('Another example of display text')
