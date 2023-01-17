cc828c60c85fb7a02d643ea05bd41d940aaa2137
bradleysnowflake <122900109+bradleysnowflake@users.noreply.github.com>
2023-01-17 11:01:43 -0600

    Update streamlit_app.py
    Add echo to both example blocks

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,19 @@ with st.echo(code_location='below'):
 
     st.write('Example Randomized Dataplot', c)
 
+with st.echo(code_location='below'):
+    # execute below to show the backend:
+    st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+    st.text('Smoothie Macro Calculator')
+    my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt', index_col = 'Fruit')
+    # Supreme overload Snowflake requests a picklist:
+    fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple','Strawberries'])
+    fruit_display = my_fruit_list.loc[fruit_selected]
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.text('Smoothie Macro Calculator')
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt', index_col = 'Fruit')
-# Supreme overload Snowflake requests a picklist:
-fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple','Strawberries'])
-fruit_display = my_fruit_list.loc[fruit_selected]
-
-if len(fruit_selected) > 0:
-    st.write(fruit_display)
-else:
-    st.write(my_fruit_list)
+    if len(fruit_selected) > 0:
+        st.write(fruit_display)
+    else:
+        st.write(my_fruit_list)
 
 
 
